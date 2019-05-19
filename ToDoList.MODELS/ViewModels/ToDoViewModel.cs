2874a499cd9b7d6b08318f8d26118153d313f03e
Mücahit Seba <mucahitseba@gmail.com>
2019-05-19 12:43:53 +0300

    Ajax ile listeleme ve arama işlemleri eklendi.

---
+++ ToDoList.MODELS/ViewModels/ToDoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.MODELS.ViewModels
+{
+    public class ToDoViewModel
+    {
+        public Guid ToDoId { get; set; } = Guid.NewGuid();
+        
+        public string BusinessName { get; set; }
+        
+        public string Description { get; set; }
+        public bool IsDone { get; set; } = false;
+        public DateTime ToDoNotifyDate { get; set; } = DateTime.Now;
+        public DateTime ToDoResultDate { get; set; } = DateTime.Now;
+    }
+}
--- ToDoList.UI/Controllers/HomeController.cs
+++ ToDoList.UI/Controllers/HomeController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ToDoList.BLL.Repository;
+using ToDoList.DAL;
 using ToDoList.MODELS.Entities;
 using ToDoList.MODELS.Models;
+using ToDoList.MODELS.ViewModels;
 
 namespace ToDoList.UI.Controllers
 {
@@ -15,6 +17,68 @@ namespace ToDoList.UI.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public JsonResult Search(string s)
+        {
+            var key = s.ToLower();
+            if (key.Length <= 2 && key != "*")
+            {
+                return Json(new ResponseData()
+                {
+                    message = "Aramak icin 2 karakterden fazlasini girin",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+            try
+            {
+                MyContext db=new MyContext();
+                List<ToDoViewModel> data;
+                if (key == "*")
+                {
+                    data = db.ToDoDbSet.OrderBy(x => x.BusinessName)
+                        .Select(x => new ToDoViewModel()
+                        {
+                            BusinessName = x.BusinessName,
+                            Description = x.Description,
+                            IsDone = x.IsDone,
+                            ToDoId = x.ToDoId,
+                            ToDoResultDate = x.ToDoResultDate,
+                            ToDoNotifyDate = x.ToDoNotifyDate
+                        }).ToList();
+                }
+                else
+                {
+                    data = db.ToDoDbSet
+                        .Where(x =>
+                            x.BusinessName.ToLower().Contains(key)
+                            || x.Description.Contains(key))
+                        .Select(x => new ToDoViewModel()
+                        {
+                            BusinessName = x.BusinessName,
+                            Description = x.Description,
+                            IsDone = x.IsDone,
+                            ToDoId = x.ToDoId,
+                            ToDoResultDate = x.ToDoResultDate,
+                            ToDoNotifyDate = x.ToDoNotifyDate
+                        })
+                        .ToList();
+                }
+                return Json(new ResponseData()
+                {
+                    message = $"{data.Count} adet kayit bulundu",
+                    success = true,
+                    data = data
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Bir hata olustu {ex.Message}",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
         }
         [HttpPost]
         public JsonResult Add(ToDo model)
@@ -44,6 +108,30 @@ namespace ToDoList.UI.Controllers
                 return Json(new ResponseData()
                 {
                     message = $"Bir hata olustu {ex.Message}",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                MyContext db = new MyContext();
+                var selected = db.ToDoDbSet.Find(id);
+                new ToDoRepo().Delete(selected);
+                
+                return Json(new ResponseData()
+                {
+                    message = $"{selected.BusinessName} ismindeki iş basariyla silindi",
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"İş silme isleminde hata {ex.Message}",
                     success = false
                 }, JsonRequestBehavior.AllowGet);
             }
